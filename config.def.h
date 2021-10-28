34e61d804aa7cdc98480c40d1d74985a95f3fb50
hoshiya4522 <dhrubodev@gmail.com>
2021-10-28 20:35:56 +0600

    MOVED TO ST

--- config.def.h
+++ config.def.h
@@ -9,9 +9,8 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
-
 static const char *fonts[] = {"agave Nerd Font:style=Regular;size=8"};
-static const char dmenufont[]       = "JetBrains Mono:size=8";
+static const char dmenufont[]       = "agave Nerd Font:style=Regular;size=8";
 
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -38,6 +37,7 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",      NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox",   NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "st",        NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "Alacritty", NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,        NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
@@ -57,6 +57,7 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
+#define ALT Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -71,13 +72,23 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+
+static const char *st[]  = { "st", NULL };
+static const char *alacritty[]  = { "alacritty", NULL };
+
+static const char *firefox[]  = { "firefox", NULL };
+static const char *screenshot[]  = { "screenshot", NULL };
+static const char *selectScreenshot[]  = { "select-screenshot", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ ALT,                          XK_b,      spawn,          {.v = firefox } },
+	{ ALT,                          XK_s,      spawn,          {.v = screenshot } },
+	{ ALT|ShiftMask,                XK_s,      spawn,          {.v = selectScreenshot } },
+
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	/* { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } }, */
+	{ MODKEY,                       XK_Return, spawn,          {.v = st } },
+	/* { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = st } }, */
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -117,9 +128,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
  	{ MODKEY,                       XK_n,      togglealttag,   {0} },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	/* { MODKEY,                       XK_minus,  setgaps,        {.i = -1 } }, */
+	/* { MODKEY,                       XK_equal,  setgaps,        {.i = +1 } }, */
+	/* { MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } }, */
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -139,7 +150,7 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	/* { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } }, */
+	/* { ClkStatusText,        0,              Button2,        spawn,          {.v = st } }, */
  	{ ClkStatusText,        0,              Button1,        sigstatusbar,   {.i = 1} },
  	{ ClkStatusText,        0,              Button2,        sigstatusbar,   {.i = 2} },
  	{ ClkStatusText,        0,              Button3,        sigstatusbar,   {.i = 3} },
--- config.h
+++ config.h
@@ -73,7 +73,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 
-static const char *st[]  = { "alacritty", NULL };
+static const char *st[]  = { "st", NULL };
 static const char *alacritty[]  = { "alacritty", NULL };
 
 static const char *firefox[]  = { "firefox", NULL };
@@ -128,9 +128,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
  	{ MODKEY,                       XK_n,      togglealttag,   {0} },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	/* { MODKEY,                       XK_minus,  setgaps,        {.i = -1 } }, */
+	/* { MODKEY,                       XK_equal,  setgaps,        {.i = +1 } }, */
+	/* { MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } }, */
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
