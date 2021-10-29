f511e463036b8b63ee9f73853edf37dc963b80a1
hoshiya4522 <dhrubodev@gmail.com>
2021-10-30 01:06:02 +0600

    idk its 1AM

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,4 @@
 bash $HOME/.scripts/vboxres &
 dwmblocks &
-feh ~/Pictures/Wallpapers/pexels-maxime-francis-2246476.jpg --bg-fill &
+feh ~/Pictures/Wallpapers/acyclovir.png --bg-fill &
 dwm >/dev/null 2>&1 &
--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const int user_bh            = 28;     
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -12,22 +12,43 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[] = {"agave Nerd Font:style=Regular;size=8"};
 static const char dmenufont[]       = "agave Nerd Font:style=Regular;size=8";
 
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+
+/* Polar Night */
+static const char nord_00[]       = "#2e3440";
+static const char nord_01[]       = "#3b4252";
+static const char nord_02[]       = "#434c5e";
+static const char nord_03[]       = "#4c566a";
+/* Snow Storm */
+static const char nord_04[]       = "#d8dee9";
+static const char nord_05[]       = "#e5e9f0";
+static const char nord_06[]       = "#eceff4";
+/* Frost */
+static const char nord_07[]       = "#8fbcbb";
+static const char nord_08[]       = "#88c0d0";
+static const char nord_09[]       = "#81a1c1";
+static const char nord_10[]       = "#5e81ac";
+/* Aurora */
+static const char nord_11[]       = "#bf616a";
+static const char nord_12[]       = "#d08770";
+static const char nord_13[]       = "#ebcb8b";
+static const char nord_14[]       = "#a3be8c";
+static const char nord_15[]       = "#b48ead";
+
+static const char tmp[]       = "";
+static const char tmp2[]      = "#8FCBD9";
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*                 fg       bg       border   */
+	[SchemeNorm]   = { nord_05, nord_00, nord_08 },
+	[SchemeSel]    = { nord_06, nord_03, nord_04 },
+	[SchemeTitle]  = { nord_06, nord_00, nord_04 },
 };
 
 /* tagging */
 static const char *tags[] = { " ", " ", " ", " ", " ", " ", "力 ", " ", " " };
-/* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
 static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const int momentaryalttags = 0; /* 1 means alttags will show only when key is held down*/
+static const int momentaryalttags = 1; /* 1 means alttags will show only when key is held down*/
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -73,8 +94,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *st[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", nord_00, "-nf", nord_05, "-sb", nord_08, "-sf", nord_06, NULL };
+/* static const char *st[]  = { "st", NULL }; */
 static const char *alacritty[]  = { "alacritty", NULL };
 static const char *firefox[]  = { "firefox", NULL };
 static const char *screenshot[]  = { "screenshot", NULL };
@@ -87,8 +108,8 @@ static Key keys[] = {
 	{ ALT|ShiftMask,                XK_s,      spawn,          {.v = selectScreenshot } },
 
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = st } },
-	/* { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = st } }, */
+	{ MODKEY,                       XK_Return, spawn,          {.v = alacritty } },
+	/* { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = alacritty } }, */
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -153,7 +174,7 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	/* { ClkStatusText,        0,              Button2,        spawn,          {.v = st } }, */
+	/* { ClkStatusText,        0,              Button2,        spawn,          {.v = alacritty } }, */
  	{ ClkStatusText,        0,              Button1,        sigstatusbar,   {.i = 1} },
  	{ ClkStatusText,        0,              Button2,        sigstatusbar,   {.i = 2} },
  	{ ClkStatusText,        0,              Button3,        sigstatusbar,   {.i = 3} },
