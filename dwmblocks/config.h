e98c27fa24eae444769476825fa52729a2adeed3
hoshiya4522 <dhrubodev@gmail.com>
2021-11-28 17:25:19 +0600

    minimal dwmblocks config

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -1,14 +1,18 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		    /*Update Interval*/	/*Update Signal*/
-    {" ",	"echo [$(\"sb-nettraf\")]",	    1,	1},
-    {"",	"sb-battery",	                5,	10},
-    {"",	"echo [墳 $(\"get-volume\")%]",	0,	2},
-    {"",	"echo [$(\"sb-clock\")]",	    60,	16},
+	{"   ",	"net-down",	    1,	0},
+    {"墳 ",	"get-volume",	0,	2},
+    {"",	"sb-battery",	    1,	0},
+    {"",	"date '+ %I:%M %p '",	    60,	0},
+    /* {" ",	"echo [$(\"sb-nettraf\")]",	    1,	1}, */
+    /* {"",	"sb-battery",	                5,	10}, */
+    /* {"",	"echo [墳 $(\"get-volume\")%]",	0,	2}, */
+    /* {"",	"echo [$(\"sb-clock\")]",	    60,	16}, */
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = "   ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
