8bc1b9283a7d1b433555e311141d7a5040a5af3f
hoshiya4522 <dhrubodev@gmail.com>
2021-12-03 16:26:51 +0600

    quickfix

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,19 @@
+#!/bin/sh
+/usr/bin/VBoxClient-all
+
+userresources=$HOME/.Xresources
+usermodmap=$HOME/.Xmodmap
+sysresources=/etc/X11/xinit/.Xresources
+sysmodmap=/etc/X11/xinit/.Xmodmap
+
+# exec awesome
+
 while true; do
-	bash $HOME/.scripts/vboxres &
-	dwmblocks &
-	feh ~/Git/dwm/wallpapers/tokyonight/tokyonight.jpg --bg-fill &
-	dwm >/dev/null 2>&1 
-done &
+bash $HOME/.scripts/vboxres &
+pkill dwmblocks &
+dwmblocks &
+dunst &
+# feh ~/Pictures/Wallpapers/acyclovir.png --bg-fill &
+feh ~/Git/dwm/wallpapers/tokyonight -z --bg-fill &
+dwm >/dev/null 2>&1 
+done
