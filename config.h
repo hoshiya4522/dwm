392ba6991f78e6f1b32e88da4f55ef5cb5848c8b
hoshiya4522 <dhrubodev@gmail.com>
2021-12-11 11:10:04 +0600

    better

--- config.h
+++ config.h
@@ -27,7 +27,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class        instance    title                  tags mask   isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",       NULL,       NULL,                  0,          1,          0,           0,        -1 },
+	/* { "Gimp",       NULL,       NULL,                  0,          1,          0,           0,        -1 }, */
+	{ "Gimp",       NULL,       NULL,                  0,          0,          0,           0,        -1 },
 	{ "Firefox",    NULL,       NULL,                  1 << 8,     0,          0,          -1,        -1 },
 	{ "st",         NULL,       NULL,                  0,          0,          1,           0,        -1 },
 	{ "Alacritty",  NULL,       NULL,                  0,          0,          1,           0,        -1 },
