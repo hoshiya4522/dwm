9462b4239b903b7dd570d8283a39d381697983d4
hoshiya4522 <dhrubodev@gmail.com>
2021-12-24 13:30:55 +0600

    ADD themes to config and SET all gaps to 10px

--- config.h
+++ config.h
@@ -1,35 +1,29 @@
 /* See LICENSE f1le for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 28;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 
-static const char *fonts[] = { "agave Nerd Font:style=Regular;size=8" };
-static const char dmenufont[]       = "agave Nerd Font:style=Regular;size=8";
+static const char *fonts[]      = { "agave Nerd Font:style=Regular;size=8" };
+static const char dmenufont[]   =   "agave Nerd Font:style=Regular;size=8";
 
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeTitle]  = { col_gray4, col_cyan,  col_cyan  },
-};
+
+/* theme */
+#include "themes/tokyonight.h"
+
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { " ", " ", " ", " ", " ", " ", "力 ", " ", " " };
 static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const int momentaryalttags = 1; /* 1 means alttags will show only when key is held down*/
 
@@ -38,18 +32,19 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "alacritty",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class        instance    title                  tags mask   isfloating   isterminal  noswallow  monitor */
+	{ "Gimp",       NULL,       NULL,                  0,          1,           0,           0,        -1 },
+	{ "firefox",    "Toolkit",  "Picture-in-Picture",  0,          1,           0,           1,        -1 },
+	{ "Firefox",    NULL,       NULL,                  1 << 8,     0,           0,          -1,        -1 },
+	{ "St",         NULL,       NULL,                  0,          0,           1,           0,        -1 },
+	{ "alacritty",  NULL,       NULL,                  0,          0,           1,           0,        -1 },
+	{ NULL,         NULL,       "Event Tester",        0,          0,           0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const float mfact        = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster        = 1;    /* number of clients in master area */
+static const int resizehints    = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
@@ -77,6 +72,7 @@ static const Layout layouts[] = {
 /* key definitions */
 #define MODKEY Mod4Mask
 #define ALT Mod1Mask
+
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -87,15 +83,39 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *alacritty[]  = { "alacritty", NULL };
+static const char *firefox[]  = { "firefox", NULL };
+static const char *screenshot[]  = { "screenshot", NULL };
+static const char *selectScreenshot[]  = { "select-screenshot", NULL };
+static const char *upvol[]   = { "vol-up",     NULL };
+static const char *downvol[]   = { "vol-down",     NULL };
+static const char *mutevol[] = { "vol-mute",  NULL };
+static const char *sd[] = { "shutdownAndOhersCTL",  NULL };
+static const char *ec[] = { "editConfig",  NULL };
+static const char *thunar[] = { "thunar",  NULL };
 
 #include "movestack.c"
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ ALT,                          XK_t,      spawn,          {.v = alacritty } },
+	{ ALT,                          XK_c,      spawn,          {.v = ec } },
+	{ ALT,                          XK_f,      spawn,          {.v = thunar } },
+	{ ALT,                          XK_b,      spawn,          {.v = firefox } },
+	{ ALT,                          XK_s,      spawn,          {.v = screenshot } },
+	{ ALT|ShiftMask,                XK_s,      spawn,          {.v = selectScreenshot } },
+	{ 0,                            XF86XK_AudioRaiseVolume,  spawn,    {.v = upvol } },
+	{ 0,                            XF86XK_AudioLowerVolume,  spawn,    {.v = downvol } },
+	{ 0,                            XF86XK_AudioMute,         spawn,    {.v = mutevol } },
+	{ ALT,                          XK_equal,  spawn,          {.v = upvol } },
+	{ ALT,                          XK_minus,  spawn,          {.v = downvol } },
+	{ ALT,                          XK_0,      spawn,          {.v = mutevol } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = sd } },
+
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,		                XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -108,7 +128,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} },
 	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} },
 	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_z, zoom,           {0} },
 	{ MODKEY|Mod1Mask,              XK_u,      incrgaps,       {.i = +1 } },
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
 	{ MODKEY|Mod1Mask,              XK_i,      incrigaps,      {.i = +1 } },
@@ -126,7 +146,7 @@ static Key keys[] = {
 	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,		                XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	/* { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, */
 	{ MODKEY,                       XK_e,      setlayout,      {.v = &layouts[1]} },
